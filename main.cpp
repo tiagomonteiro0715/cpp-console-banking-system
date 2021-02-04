9f06a73e64e9b0f6817fa3947a672f5f96e063b9
Tiago monteiro <tiago.monteiro0715@gmail.com>
2021-02-04 15:30:36 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,33 +2,30 @@
 
 using namespace std;
 
-//construir sistema bancário
 string userName;
 int choice;
 int quitChoice;
-
-void Greeting(){
-    cout << "Hi what is your name?" << endl;
-    cin >> userName; // Get user input from the keyboard
-}
+int money = 0;
 
 void menu(){
     cout << "Here is your menu " << userName << endl;
     cout << "1 - Quit bank menu" << endl;
-    cout << "2 - Deposit miney" << endl;
-    cout << "3 - withdraw money" << endl;
-    cout << "4 - show info" << endl;
+    cout << "2 - Deposit money" << endl;
+    cout << "3 - Withdraw money" << endl;
+    cout << "4 - Show info" << endl;
     cin >> choice;
 }
 
+
 void quitMenu(){
-    cout << "Are you choice" << endl;
+    cout << "---------------------------------" <<endl;
+    cout << "Do you want to quit?" << endl;
     cout << "1 - Yes" << endl;
     cout << "2 - No" << endl;
+    cout << "---------------------------------" <<endl;
     cin >> quitChoice;
-
     if (quitChoice == 1) {
-        std::exit();
+        exit (EXIT_FAILURE);
     } else if (quitChoice == 2){
         menu();
     }
@@ -36,26 +33,60 @@ void quitMenu(){
 }
 
 
+void info(){
+    cout << "Name: John" << endl;
+    cout << "Age: 33" << endl;
+    cout << "Social security number: 2093 4782 235" << endl;
+    quitMenu();
+}
+
+
+void deposit(){
+    int amount;
+    cout << "How much do you want to deposit?" <<endl;
+    cin >> amount;
+    int money = money + amount;
+    cout << "You now have " << money << " in your account" <<endl;
+    quitMenu();
+
+}
+
+void withdraw(){
+    int amount;
+    cout << "How much do you want to withdraw?" <<endl;
+    cin >> amount;
+    int money = money - amount;
+    cout << "You now have " << money << " in your account" <<endl;
+    quitMenu();
+
+}
+
+
 int main() {
-  Greeting();
-  menu();
+    cout << "--------------------------" << endl;
+    cout << "You have " << money << " in your account" << endl;
+    cout << "--------------------------" << endl;
+
+while (true) {
+
 
   switch(choice) {
   case 1:
     quitMenu();
     break;
+  case 2:
+    deposit();
+    break;
+  case 3:
+    withdraw();
+    break;
+  case 4:
+    info();
+    break;
+  default:
+    menu();
+ }
 
-  //case 2:
-    // code block
-    //break;
-  //case 3:
-    // code block
-    //break;
-  //case 4:
-    // code block
-    //break;
-  //default:
-    // code block
 }
   return 0;
 }
